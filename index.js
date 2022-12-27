d8f49a1e29fcfd71657b84e18ad07c90cdd49aae
Chiranjit Behera <chiranjitbehera34@gmail.com>
2022-12-27 22:56:09 +0530

    init

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
+const server = jsonServer.create();
+const middleWares = jsonServer.defaults();
+const router = jsonServer.router("db.json");
+
+server.use(middleWares);
 server.use(router);
 
 server.listen(3200);
